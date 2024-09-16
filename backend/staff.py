55baad9b0259d6e6f55ca9fb7a9c46fc0aa08224
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-09-17 02:29:37 +0800

    [backend] link to employee table in db done

---
+++ backend/staff.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'dbURL') or 'mysql+mysqlconnector://root:root@localhost:3306/wfh_scheduling'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+
+db = SQLAlchemy(app)
+CORS(app)
+
+
+class Staff(db.Model):
+    __tablename__ = "employee"
+
+    staff_id = db.Column(db.Integer, primary_key=True)
+    staff_fname = db.Column(db.String(50), nullable=False)
+    staff_lname = db.Column(db.String(50), nullable=False)
+    dept = db.Column(db.String(50), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    reporting_manager = db.Column(db.Integer, nullable=True)
+    role = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, staff_id, staff_fname, staff_lname, dept, position, country, email, role, reporting_manager=None):
+        self.staff_id = staff_id
+        self.staff_fname = staff_fname
+        self.staff_lname = staff_lname
+        self.dept = dept
+        self.position = position
+        self.country = country
+        self.email = email
+        self.reporting_manager = reporting_manager
+        self.role = role
+
+    def json(self):
+        return {
+            "staff_id": self.staff_id,
+            "staff_fname": self.staff_fname,
+            "staff_lname": self.staff_lname,
+            "dept": self.dept,
+            "position": self.position,
+            "country": self.country,
+            "email": self.email,
+            "reporting_manager": self.reporting_manager,
+            "role": self.role
+        }
+
+
+@app.route("/staff/<int:staff_id>")
+def get_staff_details(staff_id):
+    try:
+        # based on the email that was login, retrieve guest details
+        staff = db.session.query(Staff).filter_by(staff_id=staff_id).first()
+        if staff:
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": staff.json()
+                }
+            )
+
+    except Exception as e:
+        return jsonify({
+            "code": 404,
+            "error": "Staff not found." + str(e)
+        }), 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
