6ebe3e67c793911dcaf97d5fed782f334b5173da
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-09-18 21:15:49 +0800

    included request.py in run_flask.bat

--- backend/run_flask.bat
+++ backend/run_flask.bat
@@ -1,11 +1,11 @@
 @echo off
-
 setlocal
-
 set "FLASK_DIR=%CD%"
 
 wt.exe -w myWindow nt -p "Command Prompt" -d "%FLASK_DIR%" -- cmd /c "set FLASK_APP=employee.py && set FLASK_ENV=development && flask run --port 5000"
 
-wt.exe -w myWindow nt -p "Command Prompt" -d "%FLASK_DIR%" -- cmd /c "set FLASK_APP=request_dates.py && set FLASK_ENV=development && flask run --port 5001"
+wt.exe -w myWindow nt -p "Command Prompt" -d "%FLASK_DIR%" -- cmd /c "set FLASK_APP=request.py && set FLASK_ENV=development && flask run --port 5001"
+
+wt.exe -w myWindow nt -p "Command Prompt" -d "%FLASK_DIR%" -- cmd /c "set FLASK_APP=request_dates.py && set FLASK_ENV=development && flask run --port 5002"
 
 wt.exe -w myWindow nt -p "Command Prompt" -d "%FLASK_DIR%" -- cmd /c "set FLASK_APP=view_schedule.py && set FLASK_ENV=development && flask run --port 5100"
