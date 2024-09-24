e751f4ed92085ab41029bf09f4a9feb41a96f35c
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-09-25 00:10:22 +0800

    added to remove the error in console in browser

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/weeklycalendar',
-      name: 'weeklycalendar',
-      component: () => import('../views/WeeklyCalendar.vue')
-    }
-  ]
-})
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/weeklycalendar",
+      name: "weeklycalendar",
+      component: () => import("../views/WeeklyCalendar.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
