c461a61d7c5e33faa261b5f70ec50aed03fcd744
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-10-09 02:13:19 +0800

    move auto-reject code to calendar view, removed useless component

--- frontend/src/views/FullCalendar.vue
+++ frontend/src/views/FullCalendar.vue
@@ -1,5 +1,20 @@
 <script setup>
-
+fetch(`http://localhost:5002/request_dates/auto_reject`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(responseData => {
+    console.log('Success');
+  })
+  .catch(error => console.error('Error updating status:', error));
 </script>
 
 <template>
