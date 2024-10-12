e9318372cbb454c119221e7507a5ed670d7000c8
Denzel <117646405+denzelxk@users.noreply.github.com>
2024-10-12 23:02:17 +0800

    Update run_flask.sh

--- backend/run_flask.sh
+++ backend/run_flask.sh
@@ -6,16 +6,16 @@ FLASK_DIR=$(pwd)
 # make this shit executable: chmod +x run_flask.sh
 
 # Start the Flask applications in separate terminal windows
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "employee.py" -- bash -c "export FLASK_APP=employee.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5000"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=employee.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5000"
 
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "request.py" -- bash -c "export FLASK_APP=request.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5001"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=request.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5001"
 
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "request_dates.py" -- bash -c "export FLASK_APP=request_dates.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5002"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=request_dates.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5002"
 
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "view_schedule.py" -- bash -c "export FLASK_APP=view_schedule.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5100"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=view_schedule.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5100"
 
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "view_requests.py" -- bash -c "export FLASK_APP=view_requests.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5101"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=view_requests.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5101"
 
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "reject_requests.py" -- bash -c "export FLASK_APP=view_requests.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5102"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=view_requests.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5102"
 
-wt -w myWindow nt -p "Terminal" -d "$FLASK_DIR" --title "status_log.py" -- bash -c "export FLASK_APP=view_requests.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5003"
+open -a Terminal.app -n --args bash -c "export FLASK_APP=view_requests.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run --port 5003"
