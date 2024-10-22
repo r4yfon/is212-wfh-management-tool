99c5a2e9b4eb193c44459b3ff14b4d6fd8c41e0f
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-10-22 17:05:22 +0800

    hr view_org_sched initial file

---
+++ frontend/src/url_paths.js
@@ -0,0 +1,9 @@
+export const url_paths = {
+  employee: "http://localhost:5000/employee",
+  request: "http://localhost:5001/request",
+  request_dates: "http://localhost:5002/request_dates",
+  status_log: "http://localhost:5003/status_log",
+  view_schedule: "http://localhost:5100",
+  view_requests: "http://localhost:5101",
+  reject_requests: "http://localhost:5102/reject_request",
+};
