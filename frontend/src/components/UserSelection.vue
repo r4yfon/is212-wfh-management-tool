8946c4293066af97b8522b6b3c08f7a514ee8e55
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-10-27 20:33:44 +0800

    update user dropdown to fit various use cases

--- frontend/src/components/UserSelection.vue
+++ frontend/src/components/UserSelection.vue
@@ -12,10 +12,9 @@ const userStore = useMainStore();
     </template>
     <v-list>
       <v-list-item v-for="user in users" :key="user.staff_id" @click="selectUser(user)">
-        <v-list-item-title>
-          {{ user.staff_fname }} {{ user.staff_lname }}
-          <v-list-item-subtitle>{{ user.department }}</v-list-item-subtitle>
-        </v-list-item-title>
+        <v-list-item-title>{{ user.staff_fname }} {{ user.staff_lname }}</v-list-item-title>
+        <v-list-item-subtitle>Department: {{ user.department }}</v-list-item-subtitle>
+        <v-list-item-subtitle>Position: {{ user.position }}</v-list-item-subtitle>
       </v-list-item>
     </v-list>
   </v-menu>
@@ -26,16 +25,27 @@ export default {
   data() {
     return {
       users: [
+        // {
+        //   staff_id: 150488,
+        //   staff_fname: "Jacob",
+        //   staff_lname: "Tan",
+        //   department: "Engineering",
+        //   position: "Call Centre",
+        //   country: "Singapore",
+        //   email: "Jacob.Tan@allinone.com.sg",
+        //   reporting_manager: 151408,
+        //   role: 2,
+        // },
         {
-          staff_id: 150488,
-          staff_fname: "Jacob",
-          staff_lname: "Tan",
-          department: "Engineering",
-          position: "Call Centre",
-          country: "Singapore",
-          email: "Jacob.Tan@allinone.com.sg",
-          reporting_manager: 151408,
-          role: 2,
+          staff_id: 140937,
+          staff_fname: "Sirirat",
+          staff_lname: "Chaiyaporn",
+          department: "Sales",
+          position: "Account Manager",
+          country: "Hong Kong",
+          email: "Sirirat.Chaiyaporn@allinone.com.hk",
+          reporting_manager: 140879,
+          role: 2
         },
         {
           staff_id: 130002,
@@ -48,17 +58,29 @@ export default {
           reporting_manager: 130002,
           role: 1,
         },
+        // {
+        //   staff_id: 151408,
+        //   staff_fname: "Philip",
+        //   staff_lname: "Lee",
+        //   department: "Engineering",
+        //   position: "Director",
+        //   country: "Singapore",
+        //   email: "Philip.Lee@allinone.com.sg",
+        //   reporting_manager: 130002,
+        //   role: 1
+        // },
         {
-          staff_id: 151408,
-          staff_fname: "Philip",
-          staff_lname: "Lee",
-          department: "Engineering",
-          position: "Director",
+          staff_id: 140879,
+          staff_fname: "Siti",
+          staff_lname: "Abdullah",
+          department: "Sales",
+          position: "Sales Manager",
           country: "Singapore",
-          email: "Philip.Lee@allinone.com.sg",
-          reporting_manager: 130002,
-          role: 1
-        }
+          email: "Siti.Abdullah@allinone.com.sg",
+          reporting_manager: 140001,
+          role: 3
+        },
+
       ],
     }
   },
