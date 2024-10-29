09b070a6b247581724d8862258a0640d4e63bb3f
rayfon <75479544+r4yfon@users.noreply.github.com>
2024-10-29 21:49:39 +0800

    onyl reload page when selected user changes

--- frontend/src/components/UserSelection.vue
+++ frontend/src/components/UserSelection.vue
@@ -25,17 +25,6 @@ export default {
   data() {
     return {
       users: [
-        // {
-        //   staff_id: 150488,
-        //   staff_fname: "Jacob",
-        //   staff_lname: "Tan",
-        //   department: "Engineering",
-        //   position: "Call Centre",
-        //   country: "Singapore",
-        //   email: "Jacob.Tan@allinone.com.sg",
-        //   reporting_manager: 151408,
-        //   role: 2,
-        // },
         {
           staff_id: 140937,
           staff_fname: "Sirirat",
@@ -80,17 +69,6 @@ export default {
           reporting_manager: 130002,
           role: 1,
         },
-        // {
-        //   staff_id: 151408,
-        //   staff_fname: "Philip",
-        //   staff_lname: "Lee",
-        //   department: "Engineering",
-        //   position: "Director",
-        //   country: "Singapore",
-        //   email: "Philip.Lee@allinone.com.sg",
-        //   reporting_manager: 130002,
-        //   role: 1
-        // },
       ],
     }
   },
@@ -100,7 +78,9 @@ export default {
   methods: {
     selectUser(user) {
       const userStore = useMainStore();
-      userStore.updateUser(user);
+      if (user.staff_id !== userStore.user.staff_id) {
+        userStore.updateUser(user);
+      }
     }
   }
 }
